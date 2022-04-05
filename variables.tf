7995aa5fde61bf15bdeeba4b4a8b1e640695026d
Alexander Tratsevsky <37544209+tratsevsky@users.noreply.github.com>
2022-04-05 11:45:40 +0400

    Update variables.tf
    File updated with a relevant information

--- variables.tf
+++ variables.tf
@@ -4,12 +4,15 @@ variable "region" {
 }
 variable "project" {
     type = string
+    default = "development-345907"
 }
 variable "user" {
     type = string
+    default = "terraform_service_account"
 }
 variable "email" {
     type = string
+    default = "dj-serviceaccount@development-345907.iam.gserviceaccount.com"
 }
 variable "privatekeypath" {
     type = string
