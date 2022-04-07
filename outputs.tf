22cdc285ebe0dacbd04ed3e3f4ec1ddbabd0d47f
Alexander Tratsevsky <37544209+tratsevsky@users.noreply.github.com>
2022-04-07 17:30:02 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ip" {
-  description = IP address of the GCP Compute instance"
+  description = "IP address of the GCP Compute instance"
   value = google_compute_instance.devserver.network_interface.0.network_ip
 }
