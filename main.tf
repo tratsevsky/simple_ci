3fdd1ba24e889ecae4ce69c692d1e0da9cfd813f
Alexander Tratsevsky <37544209+tratsevsky@users.noreply.github.com>
2022-04-07 15:57:56 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,9 +68,9 @@ resource "google_compute_instance" "dev" {
         "sudo apt update",
         "sudo apt upgrade -y",
         "sudo apt install apache2 -y",
-        "sudo su",
-        "cd /var/www/html",
-        "mv index.html index.html.ORIG",
+#         "sudo su",
+#         "cd /var/www/html",
+#         "sudo mv index.html index.html.ORIG",
 #         "echo '<p> My Instance! </p>' >> /var/www/html/index.html",
     ]
   }
