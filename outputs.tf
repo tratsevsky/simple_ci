32cdce12c216593db73d0aebb0a5a5b4c587b222
Alexander Tratsevsky <37544209+tratsevsky@users.noreply.github.com>
2022-04-08 12:24:30 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "ip" {
-  description = "IP address of the GCP Compute instance"
+output "instance-id" {
+  description = "ID of the GCP Compute instance"
+  value = google_compute_instance.dev.id
+}
+
+output "private-ip" {
+  description = "Private IP address of the GCP Compute instance"
   value = google_compute_instance.dev.network_interface.0.network_ip
+}
+
+output "public-ip" {
+  description = "Public IP address of the GCP Compute instance"
+  value = google_compute_address.static.address
 }
