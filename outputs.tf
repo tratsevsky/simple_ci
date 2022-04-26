d0a3c1850d3a7693a971eec703ae7324e68dc5e4
Alexander Tratsevsky <37544209+tratsevsky@users.noreply.github.com>
2022-04-26 15:40:20 +0400

    Update outputs.tf
    all lines commented

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "instance-id" {
-  description = "ID of the GCP Compute instance"
-  value = google_compute_instance.dev[count.index].id
-}
+# output "instance-id" {
+#   description = "ID of the GCP Compute instance"
+#   value = google_compute_instance.dev.id
+# }
 
-output "private-ip" {
-  description = "Private IP address of the GCP Compute instance"
-  value = google_compute_instance.dev[count.index].network_interface.0.network_ip
-}
+# output "private-ip" {
+#   description = "Private IP address of the GCP Compute instance"
+#   value = google_compute_instance.dev.network_interface.0.network_ip
+# }
 
-output "public-ip" {
-  description = "Public IP address of the GCP Compute instance"
-  value = google_compute_address.static.address
-}
+# output "public-ip" {
+#   description = "Public IP address of the GCP Compute instance"
+#   value = google_compute_address.static.address
+# }
